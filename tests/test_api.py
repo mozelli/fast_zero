bb12661f40c1a32167d32579a6ac24af799b0446
João Mozelli Neto <joaomozelli@hotmail.com>
2026-01-07 19:08:53 -0300

    Implementando rotas CRUD

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,12 +1,85 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
-    response = client.get('/')
+def test_root_deve_retornar_ok_e_ola_mundo(client):
+    response = client.get("/")
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message':'Olá Mundo!'}
+    assert response.json() == {"message": "Olá Mundo!"}
+
+
+def test_create_user(client):
+
+    response = client.post(
+        "/users/",
+        json={
+            "username": "João Mozelli",
+            "email": "joaomozelli@gmail.com",
+            "password": "secret",
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        "username": "João Mozelli",
+        "email": "joaomozelli@gmail.com",
+        "id": 1,
+    }
+
+
+def test_read_users(client):
+    response = client.get("/users/")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "users": [
+            {
+                "username": "João Mozelli",
+                "email": "joaomozelli@gmail.com",
+                "id": 1,
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        "/users/1",
+        json={
+            "username": "João Mozelli Neto",
+            "email": "joaomozelli@gmail.com",
+            "password": "mynewpassword",
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "username": "João Mozelli Neto",
+        "email": "joaomozelli@gmail.com",
+        "id": 1,
+    }
+
+
+def test_error_update_user(client):
+    response = client.put(
+        "/users/-2",
+        json={
+            "username": "João Mozelli Neto",
+            "email": "joaomozelli@gmail.com",
+            "password": "mynewpassword",
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_delete_user(client):
+    response = client.delete("/users/1")
+
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_error_delete_user(client):
+    response = client.delete("/users/30")
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
